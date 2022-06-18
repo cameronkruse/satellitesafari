24490417d458b26a8f2191e92a9f26c7e716c869
cameronkruse <14115927+cameronkruse@users.noreply.github.com>
2022-06-18 00:47:17 -0700

    updated with basic map and globe

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 import './style.css'
 
+mapboxgl.accessToken = 'pk.eyJ1IjoiaGlnaGVzdHJvYWQiLCJhIjoiY2w0amttaWRpMDZqMDNtcGJnZng3Ym03bCJ9.ZuuRTjw9GRgHcWTzi0-CoA';
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    projection: 'globe',
+    style: 'mapbox://styles/highestroad/cl4jkkgti001o15qu2iwuc3bn', // style URL
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
+
 document.querySelector('#menu').innerHTML = `
   <h1>Satellite Safari</h1>
   <p style="font-size:12px;"> Click a button below to fly to see that animal in satellite imagery somewhere on the globe</p>
